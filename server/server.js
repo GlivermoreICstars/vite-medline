cca997b188016d395604d3f392154adf9d82ef5d
Graham Livermore <glivermore@icstars.org>
2023-11-01 17:28:07 -0500

    finallly connected db

---
+++ server/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors')
+const PORT = 3000;
+const app = express();
+const mysql = require('mysql2');
+
+
+
+
+
+// Create a MySQL database connection
+const db = mysql.createConnection({
+  host: 'localhost', // Change this to your MySQL server's host
+  user: 'root',       // Change this to your MySQL username
+  password: 'password3',   // Change this to your MySQL password
+  database: 'medline_db'    // Change this to your MySQL database name
+});
+
+// Connect to the database
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database: ' + err.stack);
+    return;
+  }
+  console.log('Connected to the database');
+});
+
+// Define a sample route that queries the database
+app.get('/', (req, res) => {
+  const sql = "SELECT * FROM users"
+  db.query(sql, (err, data) => {
+    if(err) return res.json(err);
+    return res.json(data);
+  })
+})
+
+
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
