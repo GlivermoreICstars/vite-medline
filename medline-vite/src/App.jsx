79b0cdaabe2456bbbfd10cf23507b593080feb9d
Graham Livermore <glivermore@icstars.org>
2023-11-02 14:52:47 -0500

    fixed icon and cors

--- medline-vite/src/App.jsx
+++ medline-vite/src/App.jsx
@@ -11,13 +11,14 @@ import MainHeader from './components/MainHeader';
 
 
 function App() {
-
-  useEffect(() => {
-    fetch('http://localhost:3000/scorecard')
-      .then(res => res.json())
-      .then(data => console.log('This is the scorecrad page'))
-      .catch(err => console.log(err));
-  }, [])
+  
+//use useEffect if you want to display any scorecard data to the screen.
+  // useEffect(() => {
+  //   fetch('http://localhost:4000/scorecard')
+  //     .then(res => res.json())
+  //     .then(data => console.log(data))
+  //     .catch(err => console.log(err));
+  // }, [])
 
 
   return (
