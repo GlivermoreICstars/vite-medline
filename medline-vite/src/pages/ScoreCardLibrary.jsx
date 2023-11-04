5e13181da8e2a60ae2a5cdc1df8299b57ea1c2e2
Graham Livermore <glivermore@icstars.org>
2023-11-03 23:01:21 -0500

    added animation to library page

--- medline-vite/src/pages/ScoreCardLibrary.jsx
+++ medline-vite/src/pages/ScoreCardLibrary.jsx
@@ -12,13 +12,13 @@ function ScoreCardLibrary() {
     <>
     <div className="option-container">
       <button className="option-box">Scorecard History
-        <i className="fa-solid fa-timeline fa-5x"></i>
+      <i className="fa-solid fa-clock-rotate-left fa-5x fa-fade"></i>
       </button>
       <button className="option-box">Company Templates
-        <i className="fa-regular fa-window-restore fa-5x"></i>
+        <i className="fa-regular fa-window-restore fa-5x fa-fade"></i>
       </button>
       <button onClick={() => navigate('/criteria') } className="option-box">Create new scorecard
-        <i className="fa-solid fa-gavel fa-5x"></i>
+        <i className="fa-solid fa-gavel fa-5x fa-fade"></i>
       </button>
     </div>
   </>
