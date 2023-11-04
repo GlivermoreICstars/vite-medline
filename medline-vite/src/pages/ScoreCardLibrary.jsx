02eb06fa96a63660bb96aee73dc2f3ae36b1994c
Graham Livermore <glivermore@icstars.org>
2023-11-03 22:55:12 -0500

    route to scorecard page from library-useNavigate

--- medline-vite/src/pages/ScoreCardLibrary.jsx
+++ medline-vite/src/pages/ScoreCardLibrary.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
 
 function ScoreCardLibrary() {
+
+  const navigate = useNavigate()
+
+
+
   return (
     <>
-    <div>ScoreCardLibrary</div>
+    <div className="option-container">
+      <button className="option-box">Scorecard History
+        <i className="fa-solid fa-timeline fa-5x"></i>
+      </button>
+      <button className="option-box">Company Templates
+        <i className="fa-regular fa-window-restore fa-5x"></i>
+      </button>
+      <button onClick={() => navigate('/criteria') } className="option-box">Create new scorecard
+        <i className="fa-solid fa-gavel fa-5x"></i>
+      </button>
+    </div>
   </>
 )};
 
