645602258608f746f6cc7e07ed718910dc55dca6
Graham Livermore <glivermore@icstars.org>
2023-11-07 21:00:02 -0600

    somethings off. deleted font awesome stuff

--- server/server.js
+++ server/server.js
@@ -57,7 +57,17 @@ app.post("/criteria", (req, res) => {
   console.log('Received POST request for criteria:', req.body);
   try {
     const sql = "INSERT INTO criteria (`main_criteria`, `scoring_range`, `requirements`, `level1`, `level2`, `level3`, `FLname`, `date`, `justifications`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-    const values = [req.body.main_criteria, req.body.scoring_range, req.body.requirements, req.body.level1, req.body.level2, req.body.level3, req.body.FLname, req.body.date, req.body.justifications];
+    const values = [
+      req.body.main_criteria,
+      req.body.scoring_range,
+      req.body.requirements,
+      req.body.level1,
+      req.body.level2,
+      req.body.level3,
+      req.body.FLname,
+      req.body.date,
+      req.body.justifications
+    ];
 
     db.query(sql, values, (err, data) => {
       if (err) {
@@ -73,7 +83,7 @@ app.post("/criteria", (req, res) => {
   }
 });
 
-//attempting delete request to criteria
+// attempting delete request to criteria
 app.delete('/criteria/:id', (req, res) => {
   const criteriaId = req.params.id;
 
@@ -102,25 +112,25 @@ app.get('/scorecard', (req, res) => {
 
 
 //long ass get request that I ended up not needed smh
-app.post("/scorecard", (req, res) => {
-  console.log('Received POST request for criteria:', req.body);
-  try {
-    const sql = "INSERT INTO scorecard (`scorecard_id`, `user_name`, `subject_name`, `date_created`, `criteria1`, `score1`, `justification1`, `criteria2`, `score2`, `justification2`, `criteria3`, `score3`, `justification3`,`criteria4`, `score4`, `justification4`, `criteria5`, `score5`, `justification5`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-    const values = [req.body.scorecard_id, req.body.user_name, req.body.subject_name, req.body.date_created, req.body.criteria1, req.body.score1, req.body.justification1, req.body.criteria2, req.body.score2, req.body.justification2, req.body.criteria3, req.body.score3, req.body.justification3, req.body.criteria4, req.body.score4, req.body.justification4, req.body.criteria5, req.body.score5, req.body.justification5];
+// app.post("/scorecard", (req, res) => {
+//   console.log('Received POST request for criteria:', req.body);
+//   try {
+//     const sql = "INSERT INTO scorecard (`scorecard_id`, `user_name`, `subject_name`, `date_created`, `criteria1`, `score1`, `justification1`, `criteria2`, `score2`, `justification2`, `criteria3`, `score3`, `justification3`,`criteria4`, `score4`, `justification4`, `criteria5`, `score5`, `justification5`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+//     const values = [req.body.scorecard_id, req.body.user_name, req.body.subject_name, req.body.date_created, req.body.criteria1, req.body.score1, req.body.justification1, req.body.criteria2, req.body.score2, req.body.justification2, req.body.criteria3, req.body.score3, req.body.justification3, req.body.criteria4, req.body.score4, req.body.justification4, req.body.criteria5, req.body.score5, req.body.justification5];
 
-    db.query(sql, values, (err, data) => {
-      if (err) {
-        console.error("Error in the database query:", err);
-        return res.status(500).json({ error: "Internal server error", details: err.message });
-      }
+//     db.query(sql, values, (err, data) => {
+//       if (err) {
+//         console.error("Error in the database query:", err);
+//         return res.status(500).json({ error: "Internal server error", details: err.message });
+//       }
 
-      return res.status(200).json({ message: "Template has been created" });
-    });
-  } catch (error) {
-    console.error("Error in POST operation:", error);
-    return res.status(500).json({ error: "Internal server error", details: error.message });
-  }
-});
+//       return res.status(200).json({ message: "Template has been created" });
+//     });
+//   } catch (error) {
+//     console.error("Error in POST operation:", error);
+//     return res.status(500).json({ error: "Internal server error", details: error.message });
+//   }
+// });
 
 
 app.listen(PORT, () => {
